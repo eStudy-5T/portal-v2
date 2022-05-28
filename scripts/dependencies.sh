ea65940e0f1683a972bdd6ff4ce00e4b581a5a33
tainguyent <tainguyent@kobiton.com>
2022-05-28 11:25:12 +0700

    Add missing switching node version

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -1,6 +1,8 @@
 echo "Checking Node Version"
+command -v nvm
 sudo curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 sudo nvm install 14
+nvm use 14
 sudo npm i -g yarn
 yarn
 npm rebuild node-sass
