3159d5a0bf6e0bda534fe336e0791c45e31ac779
tainguyent <tainguyent@kobiton.com>
2022-05-28 11:33:19 +0700

    rework nvm scripts

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -1,9 +1,22 @@
 echo "Checking Node Version"
-command -v nvm
-sudo curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
-sudo nvm install 14
+nvm -v | grep "0.38.0" &> /dev/null
+if [ $? == 0 ]; then
+ echo "NVM Installed"
+else
+ echo "Installing NVM"
+ sudo curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+fi
+
+nvm use 14 | grep "N/A" &> /dev/null
+if [ $? == 0 ]; then
+ echo "NVM 14 ready"
+else
+ echo "Installing NVM 14"
+ nvm install 14
+ echo "NVM 14 ready"
+fi
+
 nvm use 14
-sudo npm i -g yarn
 yarn
 npm rebuild node-sass
-echo "Node Version Check Complete"
+
