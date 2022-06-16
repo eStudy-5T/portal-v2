ba52c2ae77ba183475d4b5ccaa6cbdc09824aa81
tainguyent <tainguyent@kobiton.com>
2022-06-16 20:40:02 +0700

    Use sass

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -14,7 +14,6 @@ fi
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
-nvm install 16
 nvm use 16 | grep "N/A" &> /dev/null
 if [ $? == 0 ]; then
  echo "Installing NVM 16"
