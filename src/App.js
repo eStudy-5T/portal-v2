c84123b9f0dcaa7e0ec89aeb7eca5f0e65733774
bkhactri <bkhactri@gmail.com>
2022-06-28 00:10:47 +0700

    Fix font impact

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { Provider } from 'react-redux'
 import store from './redux'
 import { ToastContainer } from 'react-toastify'
 import AppRouter from './navigation/app-router'
+import { createTheme } from '@mui/material/styles'
+import { ThemeProvider } from '@mui/material'
 
 // Import Css Here
 import './assets/scss/style.scss'
@@ -11,19 +13,31 @@ import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 import 'react-toastify/dist/ReactToastify.css'
 
+const theme = createTheme({
+  typography: {
+    fontFamily: ['Montserrat', 'system-ui', '-apple-system', 'sans-serif'].join(
+      ','
+    )
+  }
+})
+
 function App() {
   return (
-    <Provider store={store}>
-      <Router>
-        <AppRouter />
-      </Router>
-      <ToastContainer
-        position="top-right"
-        autoClose="5000"
-        theme="light"
-        limit={5}
-      />
-    </Provider>
+    <React.StrictMode>
+      <Provider store={store}>
+        <ThemeProvider theme={theme}>
+          <Router>
+            <AppRouter />
+          </Router>
+          <ToastContainer
+            position="top-right"
+            autoClose="5000"
+            theme="light"
+            limit={5}
+          />
+        </ThemeProvider>
+      </Provider>
+    </React.StrictMode>
   )
 }
 
